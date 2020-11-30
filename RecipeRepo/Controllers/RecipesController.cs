5b49c8c16d07d63059074f32b52dddba0e57e27e
Adrian Bean <adrian.bean@nottingham.ac.uk>
2020-11-30 15:15:10 +0000

    initial project commit

---
+++ RecipeRepo/Controllers/RecipesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecipeRepo.Models;
+
+namespace RecipeRepo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Recipe> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Recipe
+            {
+                Name = "Recipe no: " + index.ToString(), Ingredients = "ingredients of recipe no: " + index.ToString(), Instructions = "instructions of recipe no: " + index.ToString()
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ RecipeRepo/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecipeRepo.Models;
+
+namespace RecipeRepo.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize (RecipeContext context)
+        {
+            if (context.Recipes.Any())
+            {
+                return;
+            }
+
+            var recipes = new Recipe[]
+            {
+                new Recipe{ Name="spaghetti", Ingredients="water, spaghetti", Instructions="just read the packet" },
+                new Recipe{ Name="meatballs", Ingredients="microwave meal meatballs", Instructions="read the packet and just shove it in the microwave" },
+                new Recipe{ Name="brownies", Ingredients="shoes, socks, clothes, local shop", Instructions="use google to find a local shop and go for a walk to buy some..." },
+                new Recipe{ Name="Chips", Ingredients="frozen chips", Instructions="put a portion on a baking tray in a preheated oven till browned and crispy" },
+                new Recipe{ Name="Pizza", Ingredients="a smart phone, tablet, or computer", Instructions="place an order with just eat and enjoy a nice chilled night in" },
+            };
+
+            foreach (Recipe recipe in recipes)
+            {
+                context.Recipes.Add(recipe);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
